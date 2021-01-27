045c829193c88d63dbd756c12bda194c0bf9662d
Beriekhoi <65053744+beriekhoi@users.noreply.github.com>
2021-01-27 09:58:52 -0600

    Main script file
    added a couple of conditionals so if the selector < 0 it sums and if it's > bryh moments array length it will substract
    ```js
        if (bryhMomentSelector < 0){
            bryhMomentSelector++
        } else if(bryhMomentSelector > bryhMoments.length){
            bryhMomentSelector--
        }
    ```

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ try {
     console.warn(('b' + 'a' + + 'a' + 'a').toLowerCase());
     let bryhMoments = ["https://cdn.discordapp.com/attachments/745207909486559313/745229456561668116/20200818_123541.jpg", "https://cdn.discordapp.com/attachments/745207909486559313/746773365934653440/20200822_185033.jpg", "https://cdn.discordapp.com/attachments/745207909486559313/746773887332647102/20200822_185238.jpg", "https://cdn.discordapp.com/attachments/745207909486559313/747139929360039985/Screenshot_20200823-190608_Maps.jpg", "https://cdn.discordapp.com/attachments/745207909486559313/766262025210298398/20201015_175340_0000.png", "https://cdn.discordapp.com/attachments/745207909486559313/766373395784663060/20201015_205251.jpg", "https://cdn.discordapp.com/attachments/745207909486559313/769266051459973160/20201023_202712.jpg", "https://cdn.discordapp.com/attachments/745207909486559313/770379767488184340/Screenshot_20201026-201233_Discord.jpg", "https://cdn.discordapp.com/attachments/745207909486559313/771312224828522506/20201029_105850.jpg", "https://cdn.discordapp.com/attachments/745207909486559313/771687007738658836/20201030_112236.jpg", "https://cdn.discordapp.com/attachments/745207909486559313/777921722304692255/Screenshot_20201116-164209_Discord.jpg", "https://cdn.discordapp.com/attachments/745207909486559313/778974625459601458/20201119_142650.jpg", "https://cdn.discordapp.com/attachments/745207909486559313/802991689416704010/20210124_210148.jpg"]
     const bryhMomentSelector = bryhMomentGenerator(1,bryhMoments.length)
+    if (bryhMomentSelector < 0){
+        bryhMomentSelector++
+    } else if(bryhMomentSelector > bryhMoments.length){
+        bryhMomentSelector--
+    }
     let bryhIMG = document.createElement("img")
     bryhIMG.setAttribute("src", bryhMoments[bryhMomentSelector])
     document.body.appendChild(bryhIMG)
