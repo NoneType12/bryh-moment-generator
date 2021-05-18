2ad467f8be5ae5de8b85898b0599bcd23e304465
Beriekhoi <65053744+NoneType12@users.noreply.github.com>
2021-05-18 09:39:46 -0700

    Descriptions again bryh

--- bryh-moments/script.js
+++ bryh-moments/script.js
@@ -1,15 +1,19 @@
 try {
     fetch('export.json').then(r=>r.json()).then(data=>{
-        var bryhMoments = data.bryhMoments
-        bryhMoments.forEach(bryhMoment => {
-            let img = document.createElement("img")
-            let space = document.createElement("br")
-            img.setAttribute("src",bryhMoment)
-            document.body.appendChild(img)
+        var bryhMoments = data.bryhMoments;
+        var descriptions = data.descriptions;
+        for (let i = 0; i <= bryhMoments.length; i+=1) {
+            let img = document.createElement("img");
+            let space = document.createElement("br");
+            var descriptionElement = document.createElement('h2');
+            img.setAttribute("src",bryhMoments[i]);
+            descriptionElement.textContent = descriptions[i];
+            document.body.appendChild(descriptionElement);
+            document.body.appendChild(img);
             for (let i = 0; i < 5; i++) {
                 document.body.appendChild(space)
             }
-        });        
+        }
     });
 } catch (err){
     console.error(err)
